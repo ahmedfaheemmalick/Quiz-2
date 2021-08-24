65567bef0999a246ac41955eaef059318ef43324
ahmedmalick <ahmedmalik2k20@gmail.com>
2021-08-24 12:11:07 +0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,42 @@
 let version = "v1";
 
-let cacheFiles = [
-    '/static/js/bundle.js',
-    '/static/js/0.chunk.js',
-    '/static/js/vendors~main.chunk.js',
-    '/static/js/main.chunk.js',
-    'https://fonts.googleapis.com/css2?family=Lato&display=swap',
-    '/static/media/bg.38c377ac.jpg',
-    '/static/media/NeonTubes2.f01f7e70.otf',
-    '/manifest.json',
-    'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple',
-    '/favicon.ico',
-    '/logo192.svg',
-    '/logo192.png',
-    '/index.html',
-    '/'
-];
+// let cacheFiles = [
+//     '/static/js/bundle.js',
+//     '/static/js/0.chunk.js',
+//     '/static/js/vendors~main.chunk.js',
+//     '/static/js/main.chunk.js',
+//     'https://fonts.googleapis.com/css2?family=Lato&display=swap',
+//     '/static/media/bg.38c377ac.jpg',
+//     '/static/media/NeonTubes2.f01f7e70.otf',
+//     '/manifest.json',
+//     'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple',
+//     '/favicon.ico',
+//     '/logo192.svg',
+//     '/logo192.png',
+//     '/index.html',
+//     '/'
+// ];
 
 this.addEventListener("install", (e) => {
     this.skipWaiting();
     e.waitUntil(
         caches.open(version).then((cache) => {
-            return cache.addAll(cacheFiles);
+            return cache.addAll([
+                '/static/js/bundle.js',
+                '/static/js/0.chunk.js',
+                '/static/js/vendors~main.chunk.js',
+                '/static/js/main.chunk.js',
+                'https://fonts.googleapis.com/css2?family=Lato&display=swap',
+                '/static/media/bg.38c377ac.jpg',
+                '/static/media/NeonTubes2.f01f7e70.otf',
+                '/manifest.json',
+                'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple',
+                '/favicon.ico',
+                '/logo192.svg',
+                '/logo192.png',
+                '/index.html',
+                '/'
+            ]);
         })
     );
 });
